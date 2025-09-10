133d47e9de8acf9b3ddba2f9845b08c81e9871f3
Adrian Rodriguez <aaadrianrodriguezmontecinos@gmail.com>
2025-09-10 12:42:06 -0400

    Refactor: se genera un id unico y se centralizan las variables de los precios

--- src/ticket.js
+++ src/ticket.js
@@ -1,11 +1,19 @@
 class Ticket {
+  static ultimoID = 0;
+  
+  static tarifaBase = 10;
+  static tarifaNocturna = 6
+  static tarifaDiario = 50;
+  static PenalidadPorPerdida = 80;
+
   constructor(HoraIngreso) {
-    this.id = 1;
+    Ticket.ultimoID++;
+    this.id = Ticket.ultimoID;
     this.HoraIngreso = HoraIngreso;
-    this.tarifaBase = 10;
-    this.tarifaNocturna = 6;
-    this.tarifaDiario = 50;
-    this.MensajePenalidad = "Aviso: En caso de perdiad, se apliacra una penalidad de Bs. 80";
+    this.tarifaBase = Ticket.tarifaBase;
+    this.tarifaNocturna = Ticket.tarifaNocturna;
+    this.tarifaDiario = Ticket.tarifaDiario;
+    this.MensajePenalidad = "Aviso: En caso de perdiad, se apliacra una penalidad de Bs. " + Ticket.PenalidadPorPerdida;
   }
   showId() {
     return this.id;
