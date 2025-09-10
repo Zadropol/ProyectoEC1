77813818b1e5f526e5572189596e953c6ad50cf3
Adrian Rodriguez <aaadrianrodriguezmontecinos@gmail.com>
2025-09-10 15:08:53 -0400

    refactor: Se mejoro la implementacion del calculo normal en base a la hora de salida y entrada, ademas de una funcion qeu convierte las horas en minutos

--- src/ticket.js
+++ src/ticket.js
@@ -15,6 +15,7 @@ class Ticket {
     this.tarifaDiario = Ticket.tarifaDiario;
     this.MensajePenalidad = "Aviso: En caso de perdiad, se apliacra una penalidad de Bs. " + Ticket.PenalidadPorPerdida;
     this.perdido = false;
+    this.HoraSalida = null;
   }
   MarcarComoPerdido() {
     this.perdido = true;
@@ -23,9 +24,52 @@ class Ticket {
     if (this.perdido) {
       return Ticket.PenalidadPorPerdida;
     }
+    return undefined;
   }
+
+  TiempoAMinutos(hora){
+    const[horas, minutos] = hora.split(":").map(Number);
+    return horas * 60 + minutos;
+  }
+
+  CalcularDiferenciaHoras(HoraIngreso, HoraSalida){
+    const minutosIngreso = this.TiempoAMinutos(HoraIngreso);
+    const minutosSalida = this.TiempoAMinutos(HoraSalida);
+
+    if (minutosSalida < minutosIngreso) {
+
+      throw new Error("Hora de salida no puede ser anterior a la entrada")
+    }
+
+    return (minutosSalida - minutosIngreso) / 60;
+  }
+
+
   CalcularCosto(HoraSalida) {
-    return "10.00";
+    if (this.perdido) {
+      return Ticket.PenalidadPorPerdida.toFixed(2);
+    }
+
+    const horasEstacionado = this.CalcularDiferenciaHoras(this.HoraIngreso, HoraSalida);
+
+    let costoTotal = 0;
+    for (let i = 0; i < Math.ceil(horasEstacionado); i++) {
+      const HoraActual = parseInt(this.HoraIngreso.split(":")[0]) + i;
+      const HorarioNocturno = (HoraActual >= 18 || HoraActual < 6);
+
+      if (HorarioNocturno) {
+        costoTotal += this.tarifaNocturna;
+      } else {
+        costoTotal += this.tarifaBase;
+      }
+    }
+
+    if (costoTotal > this.tarifaDiario) {
+      costoTotal = this.tarifaDiario;
+    }
+
+    this.HoraSalida = HoraSalida;
+    return costoTotal.toFixed(2);
   }
 
     
